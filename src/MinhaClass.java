4cfc43583ba62ddac3e2f772db502ca67a98f9eb
wellingtonadonai <134670645+wellingtonadonai@users.noreply.github.com>
2023-12-02 23:37:54 -0300

    Update MinhaClass.java
    fiz mudanca

--- src/MinhaClass.java
+++ src/MinhaClass.java
@@ -1,7 +1,7 @@
 public class MinhaClass {
 
     public static void main(String[] args) {
-        System.out.print("Olá rapaziada firmeza ta tudo bem com todos");
+        System.out.print("Olá rapaziada firmeza ta tudo bem ");
 
     }
 
