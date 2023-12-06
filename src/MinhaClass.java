66c7a3670e39e05f14cca98b9d2903ff4c7202ef
wellington <wellington.h.s.oliveira@hotmail.com>
2023-12-05 23:18:03 -0300

    metodo concatenização

--- src/MinhaClass.java
+++ src/MinhaClass.java
@@ -1,8 +1,19 @@
 public class MinhaClass {
 
     public static void main(String[] args) {
-        System.out.print("Olá rapaziada firmeza");
+        String primeiroNome = "gleison";
 
+        String segundoNome = "Andrade";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.print(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+
+        return "resultado do metodo" + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
