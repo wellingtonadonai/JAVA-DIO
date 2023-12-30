28801ea406b25809db871e36f8b26606563257ec
wellington <wellington.h.s.oliveira@hotmail.com>
2023-12-30 11:21:20 -0300

    vendo condições

--- src/wel/primeirasemana/MinhaClass.java
+++ src/wel/primeirasemana/MinhaClass.java
@@ -3,19 +3,13 @@ package primeirasemana;
 public class MinhaClass {
 
     public static void main(String[] args) {
-        String primeiroNome = "gleison";
 
-        String segundoNome = "Andrade";
+        int numero1 = 1;
+        int numero2 = 2
 
-        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-
-        System.out.print(nomeCompleto);
-
-    }
-
-    public static String nomeCompleto(String primeiroNome, String segundoNome) {
-
-        return "resultado do metodo" + primeiroNome.concat(" ").concat(segundoNome);
+        if (numero1 > numero2){
+            System.out.println("a nossa condição é falsa")
+        }
     }
 
 }
